eaf3709b819ec8f0ee4b9b0288a1b0bf2de6ceeb
Justyna Bobrowska <justynabobrowska88@gmail.com>
2018-04-05 12:25:47 +0200

    beautify code

--- components/App.js
+++ components/App.js
@@ -10,12 +10,12 @@ App = React.createClass({
         };
     },
 
-    handleSearch: function(searchingText) { // 1.
+    handleSearch: function (searchingText) { // 1.
         this.setState({
             loading: true  // 2.
         });
 
-        this.getGif(searchingText)  
+        this.getGif(searchingText)
             .then(response => {
                 const data = JSON.parse(response).data; // 4.
                 const gif = {  // 5.
@@ -29,31 +29,31 @@ App = React.createClass({
                 });
             })
             .catch(error => console.log('error'));
-        
+
     },
 
-    getGif: function(searchingText, callback) {  
-        return new Promise (function (resolve, reject) {
-        const url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
-        const xhr = new XMLHttpRequest();  
+    getGif: function (searchingText, callback) {
+        return new Promise(function (resolve, reject) {
+            const url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
+            const xhr = new XMLHttpRequest();
 
-        xhr.onload = function () {
-            if (this.status === 200) {
-                resolve(this.response);
-            } else {
-                reject (new Error(this.statusText));
-            }
-            xhr.onerror = function () {
-                reject(new Error( 
-                    `XMLHttpRequest Error: ${this.statusText}`));
+            xhr.onload = function () {
+                if (this.status === 200) {
+                    resolve(this.response);
+                } else {
+                    reject(new Error(this.statusText));
+                }
+                xhr.onerror = function () {
+                    reject(new Error(
+                        `XMLHttpRequest Error: ${this.statusText}`));
+                };
             };
-        };
-        xhr.open( 'GET', url);
-        xhr.send();
+            xhr.open('GET', url);
+            xhr.send();
         });
     },
-    
-    render: function() {
+
+    render: function () {
 
         var styles = {
             margin: '0 auto',
@@ -65,13 +65,13 @@ App = React.createClass({
             <div style={styles} className='appcontent'>
                 <h1>Wyszukiwarka GIFow!</h1>
                 <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciśnij enter, aby pobierać kolejne gify.</p>
-                <Search onSearch={this.handleSearch}/>
-            <Gif 
-                loading={this.state.loading}
-                url={this.state.gif.url}
-                sourceUrl={this.state.gif.sourceUrl}
-            />
-          </div>
+                <Search onSearch={this.handleSearch} />
+                <Gif
+                    loading={this.state.loading}
+                    url={this.state.gif.url}
+                    sourceUrl={this.state.gif.sourceUrl}
+                />
+            </div>
         );
     }
 });
--- components/Search.js
+++ components/Search.js
@@ -5,31 +5,31 @@ Search = React.createClass({
         }
     },
 
-    handleChange: function(event) {
+    handleChange: function (event) {
         var searchingText = event.target.value;
-        this.setState({searchingText: searchingText});
-    
+        this.setState({ searchingText: searchingText });
+
         if (searchingText.length > 2) {
-          this.props.onSearch(searchingText);
+            this.props.onSearch(searchingText);
         }
     },
-    
-    handleKeyUp: function(event) {
+
+    handleKeyUp: function (event) {
         if (event.keyCode === 13) {
-          this.props.onSearch(this.state.searchingText);
+            this.props.onSearch(this.state.searchingText);
         }
-    },  
+    },
 
-    render: function() {
+    render: function () {
 
-        return <input 
-                    type="text"
-                    className='appinput'
-                    onChange={this.handleChange}
-                    onKeyUp={this.handleKeyUp}
-                    placeholder="Tutaj wpisz wyszukiwaną frazę"
-                    value={this.state.searchTerm}
-                />
+        return <input
+            type="text"
+            className='appinput'
+            onChange={this.handleChange}
+            onKeyUp={this.handleKeyUp}
+            placeholder="Tutaj wpisz wyszukiwaną frazę"
+            value={this.state.searchTerm}
+        />
     }
 
 })
